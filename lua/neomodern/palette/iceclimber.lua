7d2f40baa4ef85c2a622ec272ad4691bae8ed533
Hannes <hannes@gingby.se>
2025-11-13 17:26:39 -0700

    adjust palette 1

--- lua/neomodern/palette/iceclimber.lua
+++ lua/neomodern/palette/iceclimber.lua
@@ -1,19 +1,41 @@
 local Util = require("neomodern.util")
 
 ---@type neomodern.Theme
+-- local M = {
+--     alt = "#abbceb",
+--     alt_bg = "#111113",
+--     bg = "#171719",
+--     comment = "#555568",
+--     constant = "#86a3f0",
+--     fg = "#bbbac1",
+--     func = "#cc93b8",
+--     keyword = "#8a88db",
+--     line = "#1d1d22",
+--     number = "#cfa18c",
+--     operator = "#9b99a3",
+--     property = "#629da3",
+--     string = "#6b6b99",
+--     type = "#a8a6de",
+--     visual = "#2a2a31",
+--     diag_red = "#e67e80",
+--     diag_blue = "#778fd1",
+--     diag_yellow = "#ad9368",
+--     diag_green = "#658c6d",
+-- }
+
 local M = {
     alt = "#abbceb",
     alt_bg = "#111113",
     bg = "#171719",
     comment = "#555568",
-    constant = "#86a3f0",
+    constant = "#7aa2f7", -- changed
     fg = "#bbbac1",
-    func = "#cc93b8",
-    keyword = "#8a88db",
+    func = "#f7768e", -- changed
+    keyword = "#bb9af7", -- changed
     line = "#1d1d22",
-    number = "#cfa18c",
+    number = "#cc93b8", -- changed
     operator = "#9b99a3",
-    property = "#629da3",
+    property = "#73daca", -- changed
     string = "#6b6b99",
     type = "#a8a6de",
     visual = "#2a2a31",
