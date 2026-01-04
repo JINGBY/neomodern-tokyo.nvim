1ed778ed87ea8ddcc9750a7cd1180c86ee37657c
JINGBY <hannes@gingby.se>
2026-01-04 10:46:02 +0100

    adjust palette 2

--- lua/neomodern/palette/iceclimber.lua
+++ lua/neomodern/palette/iceclimber.lua
@@ -1,27 +1,6 @@
 local Util = require("neomodern.util")
 
 ---@type neomodern.Theme
--- local M = {
---     alt = "#abbceb",
---     alt_bg = "#111113",
---     bg = "#171719",
---     comment = "#555568",
---     constant = "#86a3f0",
---     fg = "#bbbac1",
---     func = "#cc93b8",
---     keyword = "#8a88db",
---     line = "#1d1d22",
---     number = "#cfa18c",
---     operator = "#9b99a3",
---     property = "#629da3",
---     string = "#6b6b99",
---     type = "#a8a6de",
---     visual = "#2a2a31",
---     diag_red = "#e67e80",
---     diag_blue = "#778fd1",
---     diag_yellow = "#ad9368",
---     diag_green = "#658c6d",
--- }
 
 local M = {
     alt = "#abbceb",
@@ -30,10 +9,10 @@ local M = {
     comment = "#555568",
     constant = "#7aa2f7", -- changed
     fg = "#bbbac1",
-    func = "#f7768e", -- changed
+    func = "#cc93b8", -- changed
     keyword = "#bb9af7", -- changed
     line = "#1d1d22",
-    number = "#cc93b8", -- changed
+    number = "#f7768e", -- changed
     operator = "#9b99a3",
     property = "#73daca", -- changed
     string = "#6b6b99",
